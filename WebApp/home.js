7c46857bde601c53424cf222dca40fad87bd7a96
mzetina <mzetina@mit.edu>
2020-09-20 02:21:17 -0400

    Added form submit handling

---
+++ WebApp/home.js
@@ -0,0 +1,27 @@
+// /pair
+// /logout
+$('form').submit(function(event){
+    event.preventDefault();
+    let post_endpoint = $(this).attr("action")
+    let id_number = $(this).serialize();
+
+    $.ajax({
+        url: post_endpoint,
+        type: "POST",
+        success: function(data) {
+            json = toJSON(data);
+        },
+        error: function(ts){
+            alert(ts.responseText);
+        }
+    }).done(function(response){
+        json = toJSON(response);
+        status = json['status'];
+        if (choose_dir(status)){
+            location.href = 'settings.html';
+        } else{
+            console.log('here')
+            $('#idnum').html('Incorrect ID number, please try again!')
+        }
+    })
+})
